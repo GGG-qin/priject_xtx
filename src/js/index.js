49e36eadc02c8da931a664c467ab3855ee588574
GGG-qin <771101934@qq.com>
2022-12-11 16:36:28 +0800

    更新了JS BUG 和 更改了CSS文件

--- src/js/index.js
+++ src/js/index.js
@@ -17,47 +17,31 @@ let next = document.querySelector('#next');
 let lis = document.querySelectorAll('.xtx_category_super li');
 for (let i = 0; i < lis.length; i++) {
   lis[i].addEventListener('mouseenter', function () {
-    //选出唯一的一个 active类  杀掉
     document.querySelector('.xtx_category_super .active').classList.remove('active');
-    // 鼠标经过谁，谁加上active类
     this.classList.add('active');
   }
   )
 }
 
-let index = 0;  //全局变量  信号量控制器  为了给右侧按钮和左侧按钮同时使用
+let index = 0;  //信号量控制器
 
 //右侧按键
 next.addEventListener('click', function () { //右侧按钮
   index++;
 
   //复原操作
-  // 需求4：解决一个BUG
-  // 点击右侧按钮可以实现播放下一张，但是鼠标经过前面的，播放就会乱序
-  // 解决方案： 让变化量（索引号） 重新赋值为 当前鼠标经过的索引号
   index = index % sps.length;
-  console.log(index);
   let src = `./uploads/banner_${index+1}.png`
   image.src=src;
-  console.log(image.src);
-  // console.log(index)
   common();
 })
 
 //左侧按钮
 prev.addEventListener('click', function () { //右侧按钮
   index--;
-
-  //复原操作
-  // 需求4：解决一个BUG
-  // 点击右侧按钮可以实现播放下一张，但是鼠标经过前面的，播放就会乱序
-  // 解决方案： 让变化量（索引号） 重新赋值为 当前鼠标经过的索引号
   index = (sps.length + index) % sps.length;
-  console.log(index);
   let src = `./uploads/banner_${index+1}.png`
   image.src=src;
-  console.log(image.src);
-  // console.log(index)
   common();
 })
 
@@ -65,9 +49,7 @@ prev.addEventListener('click', function () { //右侧按钮
 //指示器
 for (let i = 0; i < sps.length; i++) {
   sps[i].addEventListener('mouseenter', function () {
-    //选出唯一的一个 active类  杀掉
     document.querySelector('.indicator .active').classList.remove('active');
-    // 鼠标经过谁，谁加上active类
     this.classList.add('active');
     let src = `./uploads/banner_${i+1}.png`
     image.src=src;
@@ -77,26 +59,18 @@ for (let i = 0; i < sps.length; i++) {
 }
 
 function common() {
-  //选出 index 小图片做操作
-  //选出唯一的一个 active类  杀掉
   document.querySelector('.indicator .active').classList.remove('active');
   sps[index].classList.add('active')
-
   let src = `./uploads/banner_${index+1}.png`
   image.src=src;
-  //选出 index 大图片做操作
 }
 
 //轮播图
 
-// 需求7：开启定时器
-// 其实定时器自动播放，就相当于点击了右侧按钮，此时只需要， next.click()
 let timer = setInterval(function () {
   next.click();
 }, 3000);
 
-// 鼠标经过停止定时器 （清除定时器）
-// 鼠标离开开启定时器 （开启定时器）
 image.addEventListener('mouseenter', function () {
   clearInterval(timer);
 })
